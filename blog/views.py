0e79d35cabda421caba17f8d1fe9c319ea950eac
xia0sheng <wangyouyu6@163.com>
2017-05-14 21:54:37 +0800

    add usrs views template

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from blog.models import Post
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                             status = 'published',
+                             published__year=year,
+                             published__month=month,
+                             published_day=day)
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post})
