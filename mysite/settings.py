f22a8b172ca434ff7fca460f67c1a51bc33ebb24
= <=>
2017-05-22 10:31:01 +0800

    'setting格式修正'

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '9v4f_#50)!89z6$*v!vp^+c%kx-c2qzyk-d)ebk5wq8dzh&j(^'
 DEBUG = True
 
 ALLOWED_HOSTS = ['blog.wangyouyu.com', 'localhost', '127.0.0.1']
-
 
 # Application definition
 
@@ -70,7 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
@@ -82,12 +79,11 @@ DATABASES = {
         'PASSWORD': '123456',
         # 'HOST': '101.200.238.12',
         'HOST': 'localhost',
-	    'OPTIONS': {
-		        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-	}
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -107,7 +103,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -120,7 +115,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
@@ -135,4 +129,3 @@ EMAIL_HOST_USER = 'wangyouyu6@163.com'
 EMAIL_HOST_PASSWORD = 'password'
 EMAIL_PORT = 25
 EMAIL_USE_TLS = False
-
