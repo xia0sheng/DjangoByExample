4373cb70b60dc247d3bd35cb4fda4b69614fdafb
= <=>
2017-05-22 13:38:58 +0800

    请注意下urls的[]和{}的区别，不然报错

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
-urlpatterns = {
+urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^blog/', include('blog.urls', namespace='blog', app_name='blog')),
-}
+]
