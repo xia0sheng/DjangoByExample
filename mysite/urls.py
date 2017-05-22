814162ce513256e4ce40ba03db58c5e4f6aa471d
= <=>
2017-05-22 10:38:31 +0800

    edit urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
-urlpatterns = [
+urlpatterns = {
     url(r'^admin/', admin.site.urls),
-    url(r'^blog/', include('blog.urls',
-                           namespace='blog',
-                           app_name='blog')),
-]
+    url(r'', include('blog.urls',
+                     namespace='blog',
+                     app_name='blog')),
+}
