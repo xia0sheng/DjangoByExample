c2b104320739aff7d41e10e729487886c30f54ff
= <=>
2017-05-23 11:49:05 +0800

    增加推荐功能 第二章内容结束

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from taggit.models import Tag
 from .models import Post, Comment
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Count
 
 
 def post_list(request, tag_slug=None):
@@ -48,12 +49,19 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm
+
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
                    'new_comment': new_comment,
-                   'comment_from': comment_form,})
+                   'comment_from': comment_form,
+                   'similar_posts': similar_posts})
 
 
 class PostListView(ListView):
